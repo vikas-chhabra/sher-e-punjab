262aac06473059087435e074a1618bb86e01df94
vikas-chhabra <vchhabra33@gmail.com>
2018-09-15 12:16:44 +0530

    started a project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component{
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
